5d797f85cf9db8fecafc5f8a0f4f6413d08b85a2
Prof Matias <matias@gmail.com>
2021-08-14 21:30:20 -0300

    Initial Commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-/*Copyright (C) 2019 Prof Matias Garcia para -http://www.profmatiasgarcia.com.ar- con licencia GNU GPL3. 
+/*Copyright (C) 2019 Prof Matias Garcia para -http://www.profmatiasgarcia.com.ar- con licencia GNU GPL3.
 Este programa es software libre. Puede redistribuirlo y/o modificarlo bajo los términos de la Licencia Pública General de GNU según es publicada por la Free Software Foundation, bien con la versión 3 de dicha Licencia o bien (según su elección) con cualquier versión posterior. Este programa se distribuye con la esperanza de que sea útil, pero SIN NINGUNA GARANTÍA, incluso sin la garantía MERCANTIL implícita o sin garantizar la CONVENIENCIA PARA UN PROPÓSITO ARTICULAR. Véase la Licencia Pública General de GNU para más detalles.
 Debería haber recibido una copia de la Licencia Pública General junto con este programa. Si no ha sido así ingrese a -http://www.gnu.org/licenses/-*/
 
@@ -39,3 +39,5 @@ Route::get('home', function () {
     return view('home', array('nombre' => 'Matias'));
 
 });
+
+Route::view('error', 'viewError');
